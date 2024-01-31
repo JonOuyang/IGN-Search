42f921851ac5dd84d3365b5fff8f2d1308fc8505
Jonathan Ouyang <109103876+JonOuyang@users.noreply.github.com>
2024-01-30 21:48:40 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
+import random
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from bs4 import BeautifulSoup
+import time
+from selenium.webdriver.common.actions.wheel_input import ScrollOrigin
+from selenium.webdriver import ActionChains
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
 
+from selenium.common.exceptions import TimeoutException
