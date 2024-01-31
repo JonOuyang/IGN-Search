746c69d925946c3fbbcc8e7adfdf289419a6e478
Jonathan Ouyang <109103876+JonOuyang@users.noreply.github.com>
2024-01-30 22:41:36 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import random
+import nltk
+from nltk.corpus import words
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from bs4 import BeautifulSoup
@@ -11,3 +13,23 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
 
 from selenium.common.exceptions import TimeoutException
+nltk.download('words')
+
+word_list = words.words()
+# cuts list down from 236736 -> 233080
+word_list = [word for word in word_list if 3 <= len(word) <= 16]
+available_list = []
+
+driver = webdriver.Chrome()
+
+driver.get(f'https://minecraftuuid.com/?search=Dimensionz_')
+time.sleep(2)
+
+for ign in word_list:
+    driver.get(f'https://minecraftuuid.com/?search={ign}')
+    timeout = 1
+    try:
+        element_present = EC.presence_of_element_located((By.XPATH, '//*[@id="Player-Info-Row"]/div'))
+        WebDriverWait(driver, timeout).until(element_present)
+    except:
+        available_list.append(ign)
